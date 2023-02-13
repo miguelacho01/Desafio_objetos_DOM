104068cccdd6f44c4dbc3ddb1a0115449bbb5a98
miguelacho01 <107776044+miguelacho01@users.noreply.github.com>
2023-02-13 15:35:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ function crearEsudante() {
       nota2:nota2, 
       nota3:nota3
     }
+    estudiante.push(nuevoObjeto)
 
     const promedio = (nota1 + nota2 + nota3) / 3;
     if (promedio >= 3.5 && promedio <= 5) {
